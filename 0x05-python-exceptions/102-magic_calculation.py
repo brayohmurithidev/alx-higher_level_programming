785f86081830bd548ef3c2b2353b214b0de42607
brayohmurithidev <murithibrianm@gmail.com>
2022-09-12 22:10:38 +0300

    update magic calculation

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,5 +8,5 @@ def magic_calculation(a, b):
             result += a ** b / i
         except Exception:
             result = b + a
-            break
-    return 
+            break:
+                return result
