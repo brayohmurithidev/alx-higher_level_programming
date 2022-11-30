99563304eb95d1ae5541befa1cb72091d9012a04
brayohmurithidev <murithibrianm@gmail.com>
2022-11-30 17:27:37 +0300

    function add

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
-const strToNum = Number(args[0])
+const strToNum = Number(args[0]);
 if (isNaN(strToNum)) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
   for (let i = 0; i < strToNum; i++) {
-    console.log('X'.repeat(args))
+    console.log('X'.repeat(args));
   }
 }
