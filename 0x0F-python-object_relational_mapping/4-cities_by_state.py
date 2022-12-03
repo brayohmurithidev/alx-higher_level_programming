7e433f44374888881568e68dd96dcd637db415ba
brayohmurithidev <murithibrianm@gmail.com>
2022-12-03 19:16:01 +0300

    select all cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""select all cities"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    cur = db.cursor()
+    sql = 'SELECT  *\
+         FROM cities \
+            INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC'
+    cur.execute(sql)
+    results = cur.fetchall()
+
+    for result in results:
+        print(result)
