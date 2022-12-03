fb902efbdd7bb74d95cd27979bb499ebd6b1a381
brayohmurithidev <murithibrianm@gmail.com>
2022-12-03 18:04:05 +0300

    filter records by N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Lists all states with a name starting with N from the database"""
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) >= 4:
         db = MySQLdb.connect(
             host='localhost',
@@ -11,10 +12,14 @@ if __name__ == '__main__':
             db=sys.argv[3],
             port=3306
         )
-        cur = db.cursor()
-        cur.execute(
-            "SELECT * FROM states WHERE name IS NOT NULL AND LEFT(CAST(name AS BINARY), 1) = 'N' ORDER BY states.id;")
-        records = cur.fetchall()
+        cursor = db.cursor()
+        cursor.execute("""
+        SELECT * FROM states
+        WHERE name IS NOT NULL
+        AND LEFT(CAST(name AS BINARY), 1) = 'N'
+        ORDER BY states.id;
+        """)
+        records = cursor.fetchall()
         for record in records:
             print(record)
         db.close()
