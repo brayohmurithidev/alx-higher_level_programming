377aa3199fe307dbff47bf098359312facfd9615
brayohmurithidev <murithibrianm@gmail.com>
2022-12-04 17:47:23 +0300

    modified model states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,11 +14,5 @@ class State(Base):
     __tablename__ = 'states'
 
     # Table properties
-    idd = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-    # A CONSTRUCTOR THAT IS OPRIONAL FOR PRETTIER OUTPUT
-    def __init__(self, username, password):
-        # self.user_id = user_id
-        self.username = username
-        self.password = password
