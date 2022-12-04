986d5879e7fcf3fd42011942427fa90afdf3ea46
brayohmurithidev <murithibrianm@gmail.com>
2022-12-05 00:33:18 +0300

    relationship between cities and states

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Contains the class definition of a City"""
+from relationship_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """The implementation of the City class"""
+    __tablename__ = "cities"
+    id = Column(Integer, nullable=False, autoincrement=True,
+                unique=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
