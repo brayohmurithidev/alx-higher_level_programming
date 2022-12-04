fd4bf2a6878632843294d04b750916ef4c634883
brayohmurithidev <murithibrianm@gmail.com>
2022-12-04 17:58:54 +0300

    formated model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
+"""
+Contains the class definition of a State that inherits from
+declarative_base
+"""
 from sqlalchemy import Column, String, Integer
-# Import declarative base for the classes
 from sqlalchemy.ext.declarative import declarative_base
 
-
-# base to be used for the tables/ manage tables
 Base = declarative_base()
 
 
-# Define our mapped classes
-
 class State(Base):
     __tablename__ = 'states'
-
-    # Table properties
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
